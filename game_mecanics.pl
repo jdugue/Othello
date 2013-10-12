74638b783f7e9f2d3ea1308e14c62f54be06d744
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2013-10-12 18:12:07 +0200

    add pionsJoueur

--- game_mecanics.pl
+++ game_mecanics.pl
@@ -1,4 +1,16 @@
 
-% ajoute(+Pion,+Couleur,+Plateau,-NewPlateau)
+%--------------------------------------------------
+% pionsJoueur(+Couleur, +Plateau, -PionsJoueur)
 % @Tanguy
+%
+% Récupère les pions (PionsJoueur) d'un joueur selon sa couleur (Couleur)
+%
+% ex:
+% ? - pionsJoueur(w, [[[1,1],[-1,-1]],[[1,-1],[-1,1]]], pions).
+% > pions = [[1,1],[-1,-1]]
 
+pionsJoueur(w, [PionsJ1|[PionsJ2]], PionsJ1).
+pionsJoueur(b, [PionsJ1|[PionsJ2]], PionsJ2).
+
+%------------- fin pionsJoueur() ------------------
+									
