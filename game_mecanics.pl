f1d487de5e07639b4c2c93290cc4ca789041cf86
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2013-10-12 18:12:34 +0200

    add append()

--- game_mecanics.pl
+++ game_mecanics.pl
@@ -13,4 +13,20 @@ pionsJoueur(w, [PionsJ1|[PionsJ2]], PionsJ1).
 pionsJoueur(b, [PionsJ1|[PionsJ2]], PionsJ2).
 
 %------------- fin pionsJoueur() ------------------
+
+
+%--------------------------------------------------
+% append(+Liste1,+Liste2,+ListeConcaténée)
+% @Tanguy
+%
+% Ajoute Liste2 à la suite de Liste1
+%
+% ex:
+% ? - append([a,b,c],[d],R).
+% > R = [a,b,c,d]
+
+append([ ], L1, L1).
+append([A|L1],L2,[A|L3]) :- append(L1,L2,L3).
+
+%------------- fin append() ------------------
 									
