db5a9bb61d385ba691768287976c8f0f2e9bfc29
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2013-10-21 13:44:12 +0200

    merge fix restore main.pl

--- display.pl
+++ display.pl
@@ -1,37 +1,42 @@
+:- [regles].
+:- [color].
 %--------------------------------------------------
 %	@Mohammed
 %	Affichage du plateau d'un jeu reçu
 % 	Affiche l'ensemble du plateau en variant la colonne et ligne de -N à N
-% 	PN correspond à la liste des pions noirs
-% 	PB correspond à la liste des pions blancs
+% 	PR correspond à la liste des pions rouges
+% 	PV correspond à la liste des pions verts
+%	ex : display([[[1,1],[2,1]],[[1,-1],[-4,4]]]).
 
-	display(N, PN, PB) :- N1 is N*(-1), display_output(N1, N, [N, PN, PB]).
-
+	display([PV, PR]) :- print('   A B C D E F G H\n  '), display_elem(-4, 4, [PV, PR]).
+	
 %--------------------------------------------------
 
 
 %--------------------------------------------------
 %	@Mohammed
 %	Retourne en arguments N1 et N la position des pions à afficher
+%	param1 : numéro de la colonne 
+%	param1 : numéro de la ligne
+%	param3 : liste : [pions noirs, pions blancs] 
 
-	display_output(N, N1, [N, PN, PB]) :- 
-		N1 is N*(-1),
-		!,
-		display_cell([N, N1], PN, PB),
-		write('\n\n').
-	
-	% Réaliser un retour à la ligne à la fin de la ligne
-	display_output(N, L, [N, PN, PB]) :- 
-		display_cell([N, L], PN, PB),
-		write('\n'),
-		voisin_superieur(N, L1, L), !,
-		C1 is N*(-1),
-		display_output(C1, L1, [N, PN, PB]).
-	
-	display_output(C, L, [N, PN, PB]) :- 
-		display_cell([C, L], PN, PB),
-		voisin_superieur(N, C, C1),
-		display_output(C1, L, [N, PN, PB]).
+	display_elem(4, -4, [PV, PR]) :- !, display_cell([4, -4], [PV, PR]), print('  -4\n\n').
+
+	% Si on est en fin de ligne
+	display_elem(4, L, [PV, PR]) :-
+		display_cell([4, L], [PV, PR]), 
+		print('  '),
+		print(L),
+		print('\n'), 
+		voisin_superieur(L1, L), !, % On passe à la ligne suivante
+		print('  '),
+		display_elem(-4, L1, [PV, PR]).
+		
+	% Sinon
+	display_elem(C, L, [PV, PR]) :-
+		display_cell([C, L], [PV, PR]), 
+		voisin_superieur(C, C1), % On passe à la colonne suivante
+		display_elem(C1, L, [PV, PR]).
 
 %--------------------------------------------------
 
@@ -41,17 +46,14 @@
 %	@Mohammed
 % 	Affichage d'une case
 % 	Présentation
-%		Pion noir = x
-%		Pion blanc = o
 %		Case vide = .
-
-	display_cell(C, PN, PB) :-
-		write(' '),
-		(member(C, PN) ->
-			write('x');
-			(member(C, PB) ->
-				write('o');
-				write('.')
+	display_cell(C, [PV, PR]) :-
+		print(' '),
+		(member(C, PR) ->
+			ansi_format([fg(red)], '●', []);
+			(member(C, PV) ->
+				ansi_format([fg(green)], '●', []);
+				print('.')
 			)
 		).
 
