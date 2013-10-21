4a4d3a35770efd44e08a1d75517951c74515c114
Mael <mael.ogier.38@gmail.com>
2013-10-21 17:51:11 +0200

    gestion fin de jeu

--- main.pl
+++ main.pl
@@ -8,7 +8,26 @@ playAll(Jeu) :-
 	display(Plateau),
 	jouer(Plateau,r).
 
-jouer(Plateau,Couleur) :- 
+
+%Fin de jeu, plus aucun coup légal pour les joueurs
+jouer(Plateau,Couleur) :-
+	coups_legaux(Couleur, Plateau, Coups),
+	length(Coups,L),
+	L == 0,
+	couleur_adversaire(Couleur,Adv),
+	coups_legaux(Adv, Plateau, CoupsAdv),
+	length(CoupsAdv,L2),
+	L2 == 0,
+	affichage_resultat(Plateau).
+	
+%Pas fin de jeu, mais joueur bloqué
+jouer(Plateau,Couleur) :-
+	coups_legaux(Couleur, Plateau, Coups),
+	length(Coups,L),
+	L == 0,
+	couleur_adversaire(Couleur,Adv),
+	jouer(Plateau,Adv).
+
 jouer(Plateau,Couleur) :-
 	coups_legaux(Couleur, Plateau, Coups),
 	choix_move(Coups,Choix),
@@ -16,6 +35,23 @@ jouer(Plateau,Couleur) :-
 	retourne_all(ARetourner,Plateau,PlateauTemp),
 	ajoutePion(Choix,Couleur,PlateauTemp,NewPlateau),
 	display(NewPlateau),
+	%sleep(5),
 	couleur_adversaire(Couleur,Adv),
 	jouer(NewPlateau,Adv). %On ajoute le pion que l'on a décidé de jouer
+	
+	
+affichage_resultat([J1|[J2]]) :-
+	length(J1,L1),
+	length(J2,L2),
+	write('Green='+L1),nl,
+	write('Red='+L2),nl,
+	vainqueur(L1,L2).
+	
+vainqueur(L1,L2) :-
+	L1<L2,
+	write('Red gagne !').
+	
+vainqueur(L1,L2) :-
+	L1>L2,
+	write('Green gagne !').
 	
