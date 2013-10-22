727c09aff72e33e96244a6235a4ded1bdcaaa9e7
jdugue <jossdug@gmail.com>
2013-10-22 11:44:38 +0200

    minimax du bouquin

--- ia_minimax.pl
+++ ia_minimax.pl
@@ -34,3 +34,27 @@ valeur_case(Case,Valeur) :- member(Case,[[-1,2],[1,2],[-1,-1],[-1,1],[-2,1],[-2,
 valeur_case(Case,Valeur) :- member(Case,[[-1,1],[1,-1],[1,1],[-1,-1]]),valeurs(seize,Valeur).
 
 
+/* Algo minimax d'apres le livre The Art Of Prolog */
+
+evaluate_and_choose([Move|Moves], Position,D ,MaxMin, Record, Best) :-
+	move(Move, Position, Position1), 
+	minimax(D, Position1, MaxMin, MoveX, Value), 
+	update(Move , Value , Record, Record1),
+	evaluate_and_choose([], Position, D, MaxMin, Record, Record).
+
+minimax (0, Position, MaxMin, Move, Value) :-
+	value(Position, V),
+	Value is  V*MaxMin.
+
+minimax(D, Position, MaxMin, Move, Value) :-
+	D>0, 
+	findall(M, move(Position, M), Moves),
+	D1 is D - 1, 
+	MinMax is -MaxMin, 
+	evaluate_and_choose(Moves, Position, D1, MinMax, (nil,-1000), (Move,Value)).
+
+update(Move, Value, (Move1,Value1), (Move,Value)) :-
+	Value <= Value1.	
+
+update(Move, Value, (Move1,Value1), (Move,Value)) :-
+	Value > Value1.	
