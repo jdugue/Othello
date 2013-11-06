0abb5f550e4b18bdf8a8b1adf0749c986b6f5196
jdugue <jossdug@gmail.com>
2013-11-06 09:59:58 +0100

    script test ok, joue 50 parties et donne le meilleur + script compte rendu pour faire jouer les IAs les unes contre les autres

--- scriptOthelloV2.sh
+++ scriptOthelloV2.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+nbtime=20
 red=0
 green=0
 eg=0
+pourcent=0
 
-for i in `seq 1 50`;
+for i in `seq 1 $nbtime`;
 do
 	swipl -q -s main.pl -t "playAll([ $1 , $2 ])." > temp.txt
 	tail -n 1  temp.txt >> temp2.txt
@@ -36,15 +38,25 @@ do
 done
 
 #A VERIFIER AVEC MAEL
-if $red > $green
+if [ "$red" -gt "$green" ]
 then
-	vainqueur=$1
-	pourcent=$(($red/50*100))
-else
+	# echo "1"
+	pourcent=$(($red * 100 / $nbtime))
 	vainqueur=$2
-	pourcent=$(($green/50*100))
+	# echo "pourcent "$pourcent
+elif [ "$green" -gt "$red" ]
+then
+	# echo "2"
+	pourcent=$(($green * 100 / $nbtime))
+	vainqueur=$1
+	# echo "pourcent "$pourcent
 fi
 
-echo "red "$red
-echo "green "$green
-echo "l'IA $vainqueur est la plus forte, elle a gagné dans $pourcent pourcent des parties"
+# echo "red "$red
+# echo "green "$green
+echo "\t \t--------------------------------------"
+echo "\t \t|  Match de l'IA $1 contre l'IA $2 |"
+echo "\t \t--------------------------------------"
+echo "\n"
+echo "\tl'IA $vainqueur est la plus forte, elle a gagné dans $pourcent pourcent des parties"
+echo "\n"
